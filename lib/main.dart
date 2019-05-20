d075e407b93c7e279fe6f9b56089256540567444
Daan van Marle <daanvanmarle@dsw.nl>
2019-05-20 13:53:14 +0200

    App gemaakt

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: BallPageState(),
+      backgroundColor: Colors.blue,
+    );
+  }
+}
+
+class BallPageState extends StatefulWidget {
+  @override
+  _BallPageStateState createState() => _BallPageStateState();
+}
+
+class _BallPageStateState extends State<BallPageState> {
+  var ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+          child: Image.asset('images/ball$ballNumber.png'),
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+          },
+//          highlightColor: Colors.transparent,
+//          splashColor: Colors.transparent,
+          shape: CircleBorder(
+            side: BorderSide(
+              style: BorderStyle.solid,
+              color: Colors.transparent,
+            ),
+          )),
+    );
+  }
+}
